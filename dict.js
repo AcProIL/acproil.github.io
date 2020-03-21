8133ca3f41879e5aca96845c4bfc026b6f94f0cf
AcProIL <AcProIL@mail.com>
2020-03-21 12:34:10 +0100

    Allow multiple translations

--- dict.js
+++ dict.js
@@ -1,13 +1,19 @@
 dict = [{
     "en": "sun",
-    "lsf": "sol",
+    "lsf": ["sol"],
     "examples": [
         "The sun shines. → Sol fulge."
     ]
 }, {
     "en": "shine",
-    "lsf": "fulge",
+    "lsf": ["fulge"],
     "examples": [
         "The sun shines. → Sol fulge."
+    ]
+}, {
+    "en": "man",
+    "lsf": ["homo", "homine"],
+    "examples": [
+        "Men purpose, but God disposes. → Homo propone, et Deo dispone."
     ]
 }];
